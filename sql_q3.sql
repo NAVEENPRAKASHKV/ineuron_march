8b70ffe5ba688229405a5fdec9ce59c67e1ef12e
NAVEEN PRAKASH <naveen.prakash.kv@gmail.com>
2024-03-10 10:55:13 +0530

    q3 completed

--- sql_q3.sql
+++ sql_q3.sql
@@ -1,38 +1,38 @@
 use march_3;
 
--- CREATE table users (
---     user_id int primary key,
---     username varchar(20)
--- );
+CREATE table users (
+    user_id int primary key,
+    username varchar(20)
+);
 
--- INSERT into users(user_id, username) values 
---     (1, 'John Doe'),
---     (2, 'Jane Don'),
---     (3, 'Alice Jones'),
---     (4, 'Lisa Romero');
+INSERT into users(user_id, username) values 
+    (1, 'John Doe'),
+    (2, 'Jane Don'),
+    (3, 'Alice Jones'),
+    (4, 'Lisa Romero');
 
--- CREATE table training_details (
---     user_training_id int,
---     user_id int,
---     training_id int,
---     training_date date
--- );
+CREATE table training_details (
+    user_training_id int,
+    user_id int,
+    training_id int,
+    training_date date
+);
 
--- INSERT into training_details(user_training_id, user_id, training_id, training_date) values
---     (1, 1, 1, '2015-08-02'),
---     (2, 2, 1, '2015-08-03'),
---     (3, 3, 2, '2015-08-02'),
---     (4, 4, 2, '2015-08-04'),
---     (5, 2, 2, '2015-08-03'),
---     (6, 1, 1, '2015-08-02'),
---     (7, 3, 2, '2015-08-04'),
---     (8, 4, 3, '2015-08-03'),
---     (9, 1, 4, '2015-08-03'),
---     (10, 3, 1, '2015-08-02'),
---     (11, 4, 2, '2015-08-04'),
---     (12, 3, 2, '2015-08-02'),
---     (13, 1, 1, '2015-08-02'),
---     (14, 4, 3, '2015-08-03');
+INSERT into training_details(user_training_id, user_id, training_id, training_date) values
+    (1, 1, 1, '2015-08-02'),
+    (2, 2, 1, '2015-08-03'),
+    (3, 3, 2, '2015-08-02'),
+    (4, 4, 2, '2015-08-04'),
+    (5, 2, 2, '2015-08-03'),
+    (6, 1, 1, '2015-08-02'),
+    (7, 3, 2, '2015-08-04'),
+    (8, 4, 3, '2015-08-03'),
+    (9, 1, 4, '2015-08-03'),
+    (10, 3, 1, '2015-08-02'),
+    (11, 4, 2, '2015-08-04'),
+    (12, 3, 2, '2015-08-02'),
+    (13, 1, 1, '2015-08-02'),
+    (14, 4, 3, '2015-08-03');
 
 
 SELECT u.user_id,u.username,td.training_id,COUNT(*) AS lesson_count,MAX(td.training_date) AS most_recent_lesson_date FROM  users u
---
+++ sql_q4.sql
@@ -0,0 +1,18 @@
+use march_3;
+
+-- CREATE TABLE Employee (
+--     Emp_Id INT PRIMARY KEY,
+--     Emp_Name VARCHAR(50),
+--     Salary INT
+-- );
+
+-- INSERT INTO Employee (Emp_Id, Emp_Name, Salary) VALUES
+--     (10, 'Anil', 50000),
+--     (11, 'Vikas', 75000),
+--     (12, 'Nisha', 40000),
+--     (13, 'Nidhi', 60000),
+--     (14, 'Priya', 80000),
+--     (15, 'Mohit', 45000),
+--     (16, 'Rajesh', 90000),
+--     (17, 'Raman', 55000),
+--     (18, 'Santosh', 65000);
